e4222ad0e6e1477a877020ab12ff2262242f968c
Omar López <omar.lopez@housecallpro.com>
2021-09-01 13:41:08 -0600

    Customizes message bubble grouping

--- uikit-sample/SendBirdUIKit-Sample/Customize/View/Channel/SubView/CustomUserMessageCell.swift
+++ uikit-sample/SendBirdUIKit-Sample/Customize/View/Channel/SubView/CustomUserMessageCell.swift
@@ -19,26 +19,18 @@ class CustomUserMessageCell: SBUUserMessageCell {
   override func setupStyles() {
     super.setupStyles()
 
-    // TODO: This is not working consistently
     #warning("self.mainContainerView transparent background is not working consistently")
     self.mainContainerView.leftBackgroundColor = .clear
     self.mainContainerView.leftPressedBackgroundColor = .clear
     self.mainContainerView.rightBackgroundColor = .clear
     self.mainContainerView.rightPressedBackgroundColor = .clear
-
-    switch self.position {
-      case .left:
-        bubbleImageView.image = getBubbleImage("chat_bubble_received")
-      case .right:
-        bubbleImageView.image = getBubbleImage("chat_bubble_sent")
-      default:
-        return
-    }
   }
 
   override func setupViews() {
     super.setupViews()
     self.mainContainerView.insertSubview(bubbleImageView, at: 0)
+
+    #warning("Find a way to override SBUUserMessageTextView -> Metric -> textLeftRightMargin to 23.f")
   }
 
   override func setupAutolayout() {
@@ -50,6 +42,64 @@ class CustomUserMessageCell: SBUUserMessageCell {
   override func layoutSubviews() {
     super.layoutSubviews()
     setCustomCornerRadius()
+  }
+
+  override func setMessageGrouping() {
+    guard SBUGlobals.UsingMessageGrouping else { return }
+    // Show sender profiles also
+    self.profileView.isHidden = false
+    let profileImageView = (self.profileView as? SBUMessageProfileView)?.imageView
+    let timeLabel = (self.stateView as? SBUMessageStateView)?.timeLabel
+
+    self.contentsStackView.arrangedSubviews.forEach(
+      self.contentsStackView.removeArrangedSubview(_:)
+    )
+
+    switch self.position {
+      case .left:
+        self.userNameStackView.alignment = .leading
+        self.bubbleImageView.image = getBubbleImage("chat_bubble_received")
+        self.contentsStackView.addArrangedSubview(self.profileView)
+        self.contentsStackView.addArrangedSubview(self.mainContainerView)
+        self.contentsStackView.addArrangedSubview(self.stateView)
+
+      case .right:
+        self.userNameStackView.alignment = .trailing
+        self.bubbleImageView.image = getBubbleImage("chat_bubble_sent")
+        self.contentsStackView.addArrangedSubview(self.stateView)
+        self.contentsStackView.addArrangedSubview(self.mainContainerView)
+        self.contentsStackView.addArrangedSubview(self.profileView)
+
+      case .center:
+        break
+    }
+
+    switch self.groupPosition {
+      case .top:
+        self.userNameView.isHidden = self.position == .right
+        self.bubbleImageView.image = self.position == .right
+          ? getBubbleImage("chat_bubble_sent_tailless")
+          : getBubbleImage("chat_bubble_received_tailless")
+        profileImageView?.isHidden = true
+        timeLabel?.isHidden = true
+      case .middle:
+        self.userNameView.isHidden = true
+        self.bubbleImageView.image = self.position == .right
+          ? getBubbleImage("chat_bubble_sent_tailless")
+          : getBubbleImage("chat_bubble_received_tailless")
+        profileImageView?.isHidden = true
+        timeLabel?.isHidden = true
+      case .bottom:
+        self.userNameView.isHidden = true
+        profileImageView?.isHidden = false
+        timeLabel?.isHidden = false
+      case .none:
+        self.userNameView.isHidden = self.position == .right
+        profileImageView?.isHidden = false
+        timeLabel?.isHidden = false
+    }
+
+    self.updateTopAnchorConstraint()
   }
 
   func getBubbleImage(_ name: String) -> UIImage? {
